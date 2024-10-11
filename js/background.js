4726c1995e111cf70fa791ef1b421cfef6b83c01
Michael Francis <58670911+mfrancis33@users.noreply.github.com>
2024-10-11 12:48:55 -0400

    Main extension script setup idea

---
+++ js/background.js
@@ -0,0 +1,9 @@
+// Two storage things?
+// chrome.storage.local.get/set
+// chrome.storage.sync.get/set
+
+chrome.runtime.onInstalled.addListener(function() {
+    // Stuff that runs on installation. Can perform global settings initialization here
+});
+
+
