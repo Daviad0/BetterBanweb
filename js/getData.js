9054d4ea595f550c2d82076a791b64955752c55e
M. Francis <mfrancis48439@gmail.com>
2024-10-25 12:34:18 -0400

    I think this fixes script browser compatibility

--- js/getData.js
+++ js/getData.js
@@ -1,16 +1,16 @@
-  // Function to retrieve data from local storage
-  function getData(key) {
+// Function to retrieve data from local storage
+function getData(key) {
+  let browser = window.chrome ?? window.browser;
 
-    let prom = new Promise((resolve, reject) => {
-      chrome.storage.local.get(key).then(result => {
-        console.log(`Retrieved data: ${key} = ${result[key]}`);
-        resolve(result[key]);
-      }).catch(error => {
-        console.error(`Error retrieving data: ${error}`);
-        reject(error);
-      });
+  let prom = new Promise((resolve, reject) => {
+    browser.storage.local.get(key).then(result => {
+      console.log(`Retrieved data: ${key} = ${result[key]}`);
+      resolve(result[key]);
+    }).catch(error => {
+      console.error(`Error retrieving data: ${error}`);
+      reject(error);
     });
+  });
 
-    return prom;
-  }
-  
+  return prom;
+}
--- js/saveData.js
+++ js/saveData.js
@@ -1,10 +1,11 @@
 // Function to save data to local storage
 function saveData(key, value) {
+  let browser = window.chrome ?? window.browser;
 
   let prom = new Promise((resolve, reject) => {
     const data = {};
     data[key] = value;
-    chrome.storage.local.set(data).then(() => {
+    browser.storage.local.set(data).then(() => {
       console.log(`Data saved: ${key} = ${value}`);
       resolve();
     }).catch(error => {
