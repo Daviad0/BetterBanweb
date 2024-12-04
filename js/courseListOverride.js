06274b5a6cb2c92bbb7225a8b9f2f6c04b177a14
M. Francis <mfrancis48439@gmail.com>
2024-12-04 12:07:28 -0500

    Comment courseListOverride.js

--- js/courseListOverride.js
+++ js/courseListOverride.js
@@ -1,23 +1,30 @@
 function overrideCourseListPage() {
 	const body = document.getElementById("content_body");
 
-	let cur = body.firstChild;
-	let det = null;
-	while (cur != null && cur.nodeName != "H3") {
-		cur = cur.nextSibling;
+	// Reorganize the page to hide the giant list of things under dropdowns using <details>
+
+	let curr = body.firstChild;
+	let details = null;
+	while (curr != null && curr.nodeName != "H3") {
+		// Find the first heading
+		curr = curr.nextSibling;
 	}
-	while (cur != null) {
-		const next = cur.nextSibling;
-		if (cur.nodeName == "H3") {
-			det = document.createElement("details");
-			cur.insertAdjacentElement("beforebegin", det);
-			const summ = document.createElement("summary");
-			summ.appendChild(cur);
-			det.appendChild(summ);
-			summ.classList.add("bbsummary");
+	while (curr != null) {
+		const next = curr.nextSibling;
+		if (curr.nodeName == "H3") {
+			// New heading, need to create new <details> to put stuff under
+			details = document.createElement("details");
+			curr.insertAdjacentElement("beforebegin", details);
+
+			// Make the heading the thing in the <summary> so it still shows up even if closed
+			const summary = document.createElement("summary");
+			summary.appendChild(curr);
+			details.appendChild(summary);
+			summary.classList.add("bbsummary");
 		} else {
-			det.appendChild(cur);
+			// Put the element in the <details>
+			details.appendChild(curr);
 		}
-		cur = next;
+		curr = next;
 	}
 }
